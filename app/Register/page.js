86e78ed19157eda37bca603e033bb4326629a775
unknown <naufarrelzhafifabhista@gmail.com>
2024-08-25 17:27:41 +0700

    style: fitted with ui ux

---
+++ app/Register/page.js
@@ -0,0 +1,11 @@
+"use client"
+import React from "react"
+import Signup from "@/components/SignupForm.js"
+
+export default function Register() {
+  return (
+    <section className = "flex flex-row items-center justify-center align-middle">
+      <Signup/>
+    </section>
+  )
+}
