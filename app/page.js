add49560720ce79a13679be64517b9d7ca3f8d9b
allen2610 <jessicaallen.lim@gmail.com>
2024-08-25 18:58:36 +0700

    feat:home after login

--- app/page.js
+++ app/page.js
@@ -3,7 +3,6 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="bg-white opacity-50 w-full rounded-xl m-3 p-[60px]">
-      aksdf
     </main>
   );
 }
