1600ee8efdf0ddbc6cbf42ed1e63a01959b37e80
David Lodianto <david.lodianto@gmail.com>
2024-08-25 20:58:01 +0700

    fix: fixed pagination for journalling review

--- app/Journalling/review/page.js
+++ app/Journalling/review/page.js
@@ -1,13 +1,15 @@
 import { getSession } from '@/actions/actions';
 import prisma from '@/lib/db';
-import { useRouter } from 'next/router'
+import { redirect } from 'next/navigation'
 import React from 'react'
+import PaginationControls from '@/components/PaginationControls';
 
-const JournalReview = async () => {
+const JournalReview = async ({ searchParams }) => {
     const session = await getSession();
-    const router = useRouter();
-    const { query } = router;
-    const currentCount = parseInt(query.page) || 0;
+    if(!session){
+        redirect("/")
+    }
+    const currentCount = parseInt(searchParams['page']) || '0';
     const entryPerPage = 5;
     const totalUserEntry = await prisma.journal.count({
         where: {
@@ -16,30 +18,12 @@ const JournalReview = async () => {
       })
 
     const entries = await prisma.journal.findMany({
-        skip: currentCount*5,
+        skip: Number(currentCount)*entryPerPage,
         take: entryPerPage,
         orderBy:{
             createdAt: 'desc',
         }
     })
-
-    const incrementCount = () => {
-        const newCount = currentCount + 1;
-        router.push({
-            pathname: router.pathname,
-            query: {page: newCount },
-        });
-    };
-
-    const decrementCount = () => {
-        const newCount = currentCount - 1;
-        router.push({
-            pathname: router.pathname,
-            query: {page: newCount },
-        });
-    };
-    const hasPrevPage = start > 0
-    const hasNextPage = currentCount*5 < totalUserEntry;
 
     return (
         <div>
@@ -48,8 +32,10 @@ const JournalReview = async () => {
                     <li key={entry.id}>{entry.content.slice(0, 20)}</li>
                 ))}
             </ul>
-            <button onClick={decrementCount} disabled={!hasPrevPage}>Decrement Count</button>
-            <button onClick={incrementCount} disabled={!hasNextPage}>Increment Count</button>
+            <PaginationControls
+                hasPrevPage = {currentCount > 0}
+                hasNextPage = {currentCount*5 < totalUserEntry}
+            />
         </div>
     )
 }
---
+++ components/PaginationControls.js
@@ -0,0 +1,44 @@
+'use client'
+
+import { useRouter, useSearchParams, usePathName } from 'next/navigation'
+
+const PaginationControls = (
+  {
+    hasNextPage,
+    hasPrevPage,
+  }
+) => {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+
+  const page = searchParams.get('page') ?? '0'
+  const per_page = 5
+
+  return (
+    <div className='flex gap-2'>
+      <button
+        className='bg-blue-500 text-white p-1'
+        disabled={!hasPrevPage}
+        onClick={() => {
+            router.push(`/Journalling/review?page=${Number(page) - 1}`)
+        }}>
+        prev page
+      </button>
+
+      <div>
+        {page}
+      </div>
+
+      <button
+        className='bg-blue-500 text-white p-1'
+        disabled={!hasNextPage}
+        onClick={() => {
+            router.push(`/Journalling/review?page=${Number(page) + 1}`)  
+        }}>
+        next page
+      </button>
+    </div>
+  )
+}
+
+export default PaginationControls
