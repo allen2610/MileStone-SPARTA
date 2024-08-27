e35374f1ef5e32251b4f06342cdc2682069aedca
David Lodianto <david.lodianto@gmail.com>
2024-08-28 03:23:30 +0700

    style: mood assessment fitted with ui/ux

--- components/DropdownButton.js
+++ components/DropdownButton.js
@@ -13,16 +13,16 @@ const DropdownButton = () => {
   const months = [1,2,3,4,5,6,7,8,9,10,11,12]
   const monthNames = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember']
   return (
-    <div className="dropdown">
+    <div className="dropdown bg-[#D2EBF0] rounded-3xl px-5 py-1 absolute right-20 '">
       <button onClick={toggleDropdown} className="dropdown-button">
-        Dropdown
+        ▼ Dropdown 
       </button>
       {isOpen && (
         <ul className="dropdown-menu">
           {months.map((entry) => (          
             <li key={entry.id} className="dropdown-item">
                 <button
-                className='bg-blue-500 text-white p-1'
+                className='p-1'
                 onClick={() => {
                     router.push(`/mood-Assessment?month=${entry}&year=${2024}`)
                 }}>
