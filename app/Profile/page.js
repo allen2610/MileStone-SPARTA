d9529712eb32d8405f32d591b7ceffbad221bc46
David Lodianto <david.lodianto@gmail.com>
2024-08-28 03:51:43 +0700

    feat: add dynamic data for profile

--- app/Profile/page.js
+++ app/Profile/page.js
@@ -1,9 +1,22 @@
+import { getSession } from '@/actions/actions'
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-const page = () => {
-  return (
+const page = async () => {
+    const session = await getSession();
+    if (!session){
+        redirect("/");
+    }
+    const user = session.user;
+    const dob = new Date(user.dateOfBirth);
+    const formattedDate = dob.toLocaleDateString('en-GB')
+    return (
     <div>
-      
+      Nama: {user.name}! <br />
+      Email: {user.email} <br />
+      Gender: {user.gender ? 'Laki-laki' : 'Perempuan'} <br />
+      Date Of Birth: {formattedDate}  <br />
+      Bio: {user.bio} <br />
     </div>
   )
 }
